c8829033cab0736130062ad12fec6c9127d04b8f
Ivo <ivospada03@gmail.com>
2025-05-11 18:52:55 -0300

    flujo basico terminado

--- assets/php/estado_sesion.php
+++ assets/php/estado_sesion.php
@@ -2,5 +2,6 @@
 session_start();
 echo json_encode([
     "sesion_iniciada" => isset($_SESSION['id_usuario']),
-    "usuario" => $_SESSION['nombre'] ?? null
+    "usuario" => $_SESSION['nombre'] ?? null,
+    "id" => $_SESSION['id_usuario'] ?? null
 ]);
--- assets/php/logged-resources/obtenerDatos.php
+++ assets/php/logged-resources/obtenerDatos.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 
 $id_usuario = $_SESSION['id_usuario'];
 
-$sql = "SELECT peso, altura, imc, obj_kcal, obj_fisico, nombre, apellido FROM usuario WHERE id_usuario = $id_usuario";
+$sql = "SELECT peso, altura, imc, obj_kcal, obj_fisico, nombre, apellido, registro_confirmado FROM usuario WHERE id_usuario = $id_usuario";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -31,6 +31,7 @@ if ($result->num_rows > 0) {
     $_SESSION['apellido'] = $datos['apellido'];
 
     echo json_encode([
+        'id_usuario' => $id_usuario,
         'estado' => 'ok',
         'nombre' => $datos['nombre'],
         'apellido' => $datos['apellido'],
@@ -38,7 +39,8 @@ if ($result->num_rows > 0) {
         'altura' => $datos['altura'],
         'imc' => $datos['imc'],
         'obj_kcal' => $datos['obj_kcal'],
-        'obj_fisico' => $datos['obj_fisico']
+        'obj_fisico' => $datos['obj_fisico'],
+        'registro_confirmado' => $datos['registro_confirmado']
     ]);
 } else {
     echo json_encode(['estado' => 'usuario_no_encontrado']);
